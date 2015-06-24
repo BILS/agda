f560cd2999957065d80189e2839cde7aea055d1b
Johan Viklund <johan.viklund@bils.se>
2015-06-24 14:02:55 +0000

    [species_geo_coder] Help text for the form

--- agda/species_geo_coder/forms.py
+++ agda/species_geo_coder/forms.py
@@ -3,24 +3,24 @@ from django import forms
 class SpeciesGeoCoderForm(forms.Form):
     name = forms.CharField(max_length=100, initial='SpeciesGeoCoder job', help_text='Job name')
     localities = forms.FileField(
-            help_text='',
+            help_text='Select a file containing locality data',
             label='Localities', 
             required=True)
     polygons = forms.FileField(
-            help_text='',
+            help_text='Select a file containing polygon data',
             label='Polygons', 
             required=True)
     occurences = forms.IntegerField(
-            help_text='',
+            help_text='Specify the minimum number of occurrences (localities) needed for considering a species to be present in a polygon.',
             initial=1,
             required=False,
             min_value=1,
             label='Occurence cutoff')
     verbose = forms.BooleanField(
-            help_text='',
+            help_text='Checking this box will make SpeciesGeoCoder also report how many times a species is found in each polygon.',
             required=False,
             label='Verbose')
     plot = forms.BooleanField(
-            help_text='',
+            help_text='In addition to the occurrence result in NEXUS format, this function will make SpecieGeoCoder also produce graphical output that illustrates coexistance of species, distribution etc. This function is only available for datasets containing XX or less taxon names.',
             required=False,
             label='Make plots')
