be4e4e46abf272916fb298ceb9f536c52fb8a40d
Johan Viklund <johan.viklund@bils.se>
2015-10-01 10:15:36 +0200

    [speciesgeocoder] Help view shows main view

--- agda/species_geo_coder/urls.py
+++ agda/species_geo_coder/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from .views import IndexView, ToolView
 
 urlpatterns = patterns('',
-    url(r'^$', IndexView.as_view(), name='species_geo_coder.views.top'),
-    url(r'^/tool$', ToolView.as_view(), name='species_geo_coder.views.tool_1'),
-    #(r'^mdr/scan/$', 'scan'),
+    url(r'^/?$', IndexView.as_view(), name='species_geo_coder.views.top'),
+    url(r'^/help/?$', IndexView.as_view(), name='species_geo_coder.views.top'),
+    url(r'^/tool/?$', ToolView.as_view(), name='species_geo_coder.views.tool_1'),
 )
